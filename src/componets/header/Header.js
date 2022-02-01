26aef3737e0506cac33c54fb9f40f7e217a3da13
Lakshmi5524 <webtech5524@gmail.com>
2022-02-01 10:57:19 +0530

    first commit

---
+++ src/componets/header/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './header.css'
+const Header = () => {
+  return <span onClick={()=>window.scroll(0,0)}className='header'>Entertainment Series</span>
+};
+
+export default Header;
